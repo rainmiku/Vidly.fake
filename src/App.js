a57c8dceb945e6e805c3dc08c3cab99b35bb7b53
Rainatsu <yu.xia@ar.c.titech.ac.jp>
2020-01-14 16:53:31 +0900

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFount from "./components/notFount.jsx";
 
-function App() {
-  return (
-    <main className="container">
-      <Movies />
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFount}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="not-found" />
+        </Switch>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/notFount.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFount = () => {
+  return <h1>not found</h1>;
+};
+
+export default NotFount;
